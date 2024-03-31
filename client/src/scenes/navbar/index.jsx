0230bd70dc3039c3f016d1edcf124f42114d836e
THIENNHAN <93808313+lamnhann@users.noreply.github.com>
2024-03-31 14:38:54 +0700

    Task Complete: - Register, Login Pages, and Form. - Fix (user fullName)

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -39,8 +39,8 @@ const Navbar = () => {
     const primaryLight = theme.palette.primary.light;
     const alt = theme.palette.background.alt;
 
-    // const fullName = `${user.firstName} ${user.lastName}`;
-    const fullName = `Thien Nhan`;
+    const fullName = `${user.firstName} ${user.lastName}`;
+    // const fullName = `Thien Nhan`;
 
     return <FlexBetween padding="1rem 6%" backgroundColor={alt}>
         <FlexBetween gap="1.75rem">
